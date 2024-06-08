b4a94805cb75cdf5665aea57ab56240d229bf779
stevetom1 <tomasdavidek126@gmail.com>
2024-06-08 16:13:57 +0200

    no errors :)

--- Assets/Scripts/Player/PlayerLives.cs
+++ Assets/Scripts/Player/PlayerLives.cs
@@ -1,51 +1,76 @@
 using UnityEngine;
 using TMPro;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class PlayerLives : MonoBehaviour
 {
     [SerializeField] public static int maxHp = 3;
-    int hp;
-    float bulletTimer;
+    private int hp;
+    private bool isInvincible = false;
+    private float invincibilityTimer = 0f;
+    private float invincibilityDuration = 2.0f;
 
     public GameObject[] hearts;
-
     public TextMeshProUGUI hpText;
-
     public GameObject deathScreen;
     public GameObject player;
 
     void Start()
     {
         hp = maxHp;
+        UpdateHud();
     }
 
     void Update()
     {
-        //hpText.text = "HP: " + hp + "/" + maxHp;
-        //xpText.text = "XP: " + exp;
-        //levelText.text = "Level: " + level;
+        // Handle the invincibility timer
+        if (isInvincible)
+        {
+            invincibilityTimer -= Time.deltaTime;
+            if (invincibilityTimer <= 0f)
+            {
+                isInvincible = false;
+            }
+        }
+
+        // Update HUD text if needed
+        // hpText.text = "HP: " + hp + "/" + maxHp;
+        // xpText.text = "XP: " + exp;
+        // levelText.text = "Level: " + level;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Enemy" || collision.tag == "enemyBullet")
+        if ((collision.tag == "Enemy" || collision.tag == "enemyBullet") && !isInvincible)
         {
             int damageAmount = 1;
-            GameManager.instance.PlayerTakeDamage(damageAmount);
+            TakeDamage(damageAmount);
         }
     }
 
-
-
- 
-
-
     public void TakeDamage(int damage)
     {
+        if (isInvincible)
+            return;
+
         hp -= damage;
+        UpdateHud();
+
+        if (IsDead())
+        {
+            // Handle player death (show death screen, etc.)
+            deathScreen.SetActive(true);
+            player.SetActive(false);
+            // You can also add additional logic here
+        }
+        else
+        {
+            // Activate invincibility and start the timer
+            isInvincible = true;
+            invincibilityTimer = invincibilityDuration;
+        }
     }
+
     public bool IsDead()
     {
         return hp <= 0;
@@ -58,28 +83,12 @@ public class PlayerLives : MonoBehaviour
 
     public void UpdateHud()
     {
-        if (hp < 1)
+        // Update the heart icons based on current hp
+        for (int i = 0; i < hearts.Length; i++)
         {
-            Destroy(hearts[0].gameObject);
-        }
-        else if(hp < 2)
-        {
-            Destroy(hearts[1].gameObject);
-        }
-        else if(hp < 3)
-        {
-            Destroy(hearts[2].gameObject);
+            hearts[i].SetActive(i < hp);
         }
     }
-
-
-
-
-
-
-
-
-
 
     public void Retry()
     {
